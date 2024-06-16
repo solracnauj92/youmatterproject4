cdcd94cabf60a3ee3f4cfed85f19306951650ed3
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-06-16 14:41:37 +0000

    Create class meta for models.py

--- main_page/models.py
+++ main_page/models.py
@@ -17,6 +17,12 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
 class Comment(models.Model):
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name="comments")
@@ -25,3 +31,9 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
