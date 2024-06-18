7f1a94818aceedc4c7c3e44f124085c8bcc40238
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-06-18 23:31:37 +0000

    Add comment form and enable posting a comment as authenticated user

--- main_page/views.py
+++ main_page/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Post
+from .forms import CommentForm
 from django.views.generic import ListView
 
 
@@ -26,6 +28,19 @@ def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug, status=1)
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+    )
+
+    comment_form = CommentForm()
 
     return render(
         request,
@@ -34,5 +49,6 @@ def post_detail(request, slug):
             'post': post,
             "comments": comments,
             "comment_count": comment_count,
+            "comment_form": comment_form,
         }
     )
