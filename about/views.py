31de148ccebab2cedc7006ec0b6fb0b5b4ae08e0
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-06-19 01:45:47 +0000

    Add view for handling form submission in about/views.py

--- about/views.py
+++ about/views.py
@@ -6,6 +6,12 @@ from .forms import CollaborateForm
 
 # Create your views here.
 def about_me(request):
+
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(request, messages.SUCCESS, "Collaboration request received! I endeavour to respond within 2 working days.")
     about = About.objects.all().order_by("-updated_on").first()
     collaborate_form = CollaborateForm()
     
