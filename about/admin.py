d425243fe98d715d152b12fd7a47abaeb7f44e65
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-06-19 01:40:42 +0000

    Create request form for about page

--- about/admin.py
+++ about/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import About
 from django_summernote.admin import SummernoteModelAdmin
+from .models import CollaborateRequest
 
 # Register your models here.
 @admin.register(About)
@@ -8,3 +9,8 @@ class AboutAdmin(SummernoteModelAdmin):
 
     summernote_fields = ('content',)
 # Register your models here.
+
+@admin.register(CollaborateRequest)
+class CollaborateRequestAdmin(admin.ModelAdmin):
+
+    list_display = ('message', 'read',)
--- about/views.py
+++ about/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.contrib import messages
 from .models import About
+from .forms import CollaborateForm
 
 
 # Create your views here.
 def about_me(request):
     about = About.objects.all().order_by("-updated_on").first()
+    collaborate_form = CollaborateForm()
     
     if request.method == "POST":
         # Process any POST request data here if needed
@@ -13,4 +15,5 @@ def about_me(request):
     
     return render(request, "about/about.html", {
         "about": about,
+        "collaborate_form": collaborate_form
     })
