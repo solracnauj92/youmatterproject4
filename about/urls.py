f4c5de88affeb607c507e6c40e0c52c76d9f5d0a
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-06-21 12:31:53 +0000

    Add admin-only collab request approval

--- about/urls.py
+++ about/urls.py
@@ -3,4 +3,14 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.about_me, name='about'),
+    path(
+        'collaboration_requests/',
+        views.collaboration_requests,
+        name='collaboration_requests'
+    ),
+    path(
+        'approve_collaboration_request/<int:id>/',
+        views.approve_collaboration_request,
+        name='approve_collaboration_request'
+    ),
 ]
--- about/views.py
+++ about/views.py
@@ -1,27 +1,70 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
-from .models import About
+from django.contrib.auth.models import User
+from .models import About, CollaborateRequest
 from .forms import CollaborateForm
+from main_page.models import Post
 
 
-# Create your views here.
 def about_me(request):
-
+    if request.user.is_authenticated:
+        user = get_object_or_404(User, username=request.user.username)
+        username = user.username if user.username else ""
+        email = user.email if user.email else ""
+        collaborate_form = CollaborateForm(
+            request.POST or None, initial={'name': username, 'email': email}
+        )
+    else:
+        collaborate_form = CollaborateForm(request.POST or None)
     if request.method == "POST":
-        collaborate_form = CollaborateForm(data=request.POST)
         if collaborate_form.is_valid():
+            collaborate_form.instance.name = request.user.username
             collaborate_form.save()
-            messages.add_message(request, messages.SUCCESS, "Collaboration request received! I endeavour to respond within 2 working days.")
-            return redirect('about_me')
+            messages.success(
+                request,
+                "Collaboration request received! I endeavour to respond within 2 working days.")  # noqa
+            return redirect('about')
     
     about = About.objects.all().order_by("-updated_on").first()
-    collaborate_form = CollaborateForm()
-    
-    if request.method == "POST":
-        # Process any POST request data here if needed
-        messages.add_message(request, messages.SUCCESS, "Message received!")
     
     return render(request, "about/about.html", {
         "about": about,
         "collaborate_form": collaborate_form
     })
+
+
+def collaboration_requests(request):
+    if not request.user.is_superuser:
+        messages.error(request, "Access denied - invalid credentials")
+        return redirect('about')
+
+    collaborations = CollaborateRequest.objects.filter(approved=False)
+    template = "about/collaboration_requests.html"
+    context = {
+        "collaborations": collaborations,
+    }
+
+    return render(request, template, context)
+
+
+def approve_collaboration_request(request, id):
+    if not request.user.is_superuser:
+        messages.error(request, "Access denied - invalid credentials")
+        return redirect('about')
+
+    # find the request, and approve it
+    collaboration = get_object_or_404(CollaborateRequest, id=id)
+    collaboration.approved = True
+    collaboration.save()
+
+    # create a new instance of the Post model using the requested collab data
+    Post.objects.create(
+        title="Pending Title",
+        slug=f"pending-title-{id}",
+        author=get_object_or_404(User, username=collaboration.name),
+        content=collaboration.message,
+        status=1,
+        tag=collaboration.tag,
+    )
+    messages.success(request, 'Collaboration successfully approved')
+    return redirect('collaboration_requests')
