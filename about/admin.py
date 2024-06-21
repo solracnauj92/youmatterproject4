5eafc815c6136832d42f83f8ae7a79102b0052bb
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-06-21 12:30:47 +0000

    Update newletter model and form

--- about/admin.py
+++ about/admin.py
@@ -3,15 +3,13 @@ from .models import About
 from django_summernote.admin import SummernoteModelAdmin
 from .models import CollaborateRequest
 
-# Register your models here.
+
 @admin.register(About)
 class AboutAdmin(SummernoteModelAdmin):
-
     summernote_fields = ('content',)
-# Register your models here.
+
 
 @admin.register(CollaborateRequest)
 class CollaborateRequestAdmin(admin.ModelAdmin):
-
-    list_display = ('message', 'read',)
+    list_display = ('name', 'email', 'message', 'approved', 'tag')
 
--- about/models.py
+++ about/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
-
+from main_page.models import Tag
 
 
 # Create your models here.
@@ -19,7 +19,8 @@ class CollaborateRequest(models.Model):
     name = models.CharField(max_length=200)
     email = models.EmailField()
     message = models.TextField()
-    read = models.BooleanField(default=False)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    approved = models.BooleanField(default=False)
 
     def __str__(self):
         return f"Collaboration request from {self.name}"
