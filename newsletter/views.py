20c060c674d6c670fc9b3dd0568b2fd97d02de8d
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-06-21 12:33:32 +0000

    Fix newsletter submission form

--- newsletter/views.py
+++ newsletter/views.py
@@ -2,18 +2,17 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import NewsletterSignupForm
 
-def newsletter_signup(request):
-    if request.method == 'POST':
-        form = NewsletterSignupForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'You have successfully signed up for the newsletter!')
-            return redirect('about')  
-    else:
-        form = NewsletterSignupForm()
-    return render(request, 'newsletter/newsletter_signup.html', {'form': form})
 
 def connect_view(request):
-    return render(request, 'newsletter/connect.html')
+    form = NewsletterSignupForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request,
+                'You have successfully signed up for the newsletter!')
+            return redirect('connect')  
+    
+    return render(request, 'newsletter/connect.html', {'form': form}) 
 
  
