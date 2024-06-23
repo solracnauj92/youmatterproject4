e332b70fad2e58ba27505a9f9e29e09c8bfb4c49
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-06-23 01:37:17 +0000

    First attempt to fix edit profile

--- main_page/views.py
+++ main_page/views.py
@@ -152,3 +152,17 @@ def profile(request):
         form = ProfileForm(instance=profile)
     
     return render(request, 'profile.html', {'form': form})   
+
+@login_required
+def edit_profile(request):
+    profile, created = Profile.objects.get_or_create(user=request.user)
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = ProfileForm(instance=profile)
+
+    return render(request, 'profile.html', {'form': form})
